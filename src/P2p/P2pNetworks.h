9eaf8015510116f2fdeeb12cf58be28bed5a5126
srgtcauliflour <twitchmeister@Twitchs-MacBook-Pro.local>
2019-05-10 00:26:47 +0930

    Updates

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xA0, 0x1A, 0xA1, 0x1A, 0x0A, 0xA1, 0x0A, 0xA0, 0x0A, 0xA1, 0x0A, 0xA0, 0xAA, 0xAA, 0xAA } };
+}
